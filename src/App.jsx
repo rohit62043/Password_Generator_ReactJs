cdb25510f2fb80c20a51883f100569727b2ef01c
Rohit <singhrohit51967@gmail.com>
2023-10-27 08:48:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+
+function App() {
+
+  return (
+
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+
+
+        </div>
+        <div className="flex items-center gap-x-1">
+
+
+        </div>
+        <div className="flex items-center gap-x-1">
+
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
